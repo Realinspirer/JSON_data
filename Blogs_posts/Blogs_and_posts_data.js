663c57706d93c5ed0fcc3cf68a323674c12f9720
Realinspirer <realinspirer@outlook.com>
2024-12-09 02:03:02 +0530

    Added tags

--- Blogs_posts/Blogs_and_posts_data.js
+++ Blogs_posts/Blogs_and_posts_data.js
@@ -18,7 +18,8 @@ Blogs_and_posts_data.push({
             click_url: "https://youtu.be/6wR1wl_W2F4",
             external: true
         }
-    ]
+    ],
+    tags: "VideoEditing, DaVinciResolve, YouTube, Talking, Podcast, Meme"
 });
 Blogs_and_posts_data.push({
     title: "3D Background Design for Upcoming Video [Age Shenanigans]",
@@ -29,7 +30,8 @@ Blogs_and_posts_data.push({
         "/Blogs/Images/age_video/back/1.jpg",
         "/Blogs/Images/age_video/back/2.jpg",
         "/Blogs/Images/age_video/back/3.jpg",
-    ]
+    ],
+    tags: "Blender, 3D, Shading, ShaderEditor, VFX, Effects, Background, VideoEditing, YouTube"
 });
 Blogs_and_posts_data.push({
     title: "Today's my birthday! 🎂🎂",
@@ -37,7 +39,8 @@ Blogs_and_posts_data.push({
     date: "10th November 2024",
     imgs: [
         "/Blogs/Images/cover/0.jpg"
-    ]
+    ],
+    tags: "Blender, GameDev, GameDevelopment, Unity, 3D, 3DModelling, VFX, Inspiration, Birthday"
 });
 Blogs_and_posts_data.push({
     title: "Losing my aura at a Domino's",
@@ -63,7 +66,8 @@ Blogs_and_posts_data.push({
             click_url: "https://youtube.com/shorts/YTIzMCjtuBA",
             external: true
         }
-    ]
+    ],
+    tags: "Experience, RealLife, VideoEditing, DaVinciResolve, Video, Talking, VFX, YouTube"
 });
 Blogs_and_posts_data.push({
     title: "Background Design for Upcoming Video (Domino\'s experience)",
@@ -80,7 +84,8 @@ Blogs_and_posts_data.push({
             click_url: "https://www.linkedin.com/posts/realinspirer_blender-shaders-activity-7253472608094961664-vOdn",
             external: true
         }
-    ]
+    ],
+    tags: "3D, Shading, ShaderEditor, Modelling, VFX, Background, VideoEditing, YouTube"
 });
 Blogs_and_posts_data.push({
     title: "Dwelling into 4th Dimension and Time",
@@ -105,7 +110,8 @@ Blogs_and_posts_data.push({
             click_url: "https://www.youtube.com/shorts/qtudAjtOKYQ",
             external: true
         }
-    ]
+    ],
+    tags: "VideoEditing, VFX, Informational, Science, DaVinciResolve, Blender, Talking, YouTube, Video"
 });
 Blogs_and_posts_data.push({
     title: "Thumbnail Design in DaVinci Resolve (4th Dimension Video)",
@@ -121,7 +127,8 @@ Blogs_and_posts_data.push({
     imgs: [
         "/Blogs/Images/fourth_dimension_video/thumbnail.jpg",
         "/Blogs/Images/fourth_dimension_video/thumbnail_vert.png"
-    ]
+    ],
+    tags: "DaVinciResolve, Vector, AssetDesign, VFX, VideoEditing, YouTube, ThumbnailDesign"
 });
 Blogs_and_posts_data.push({
     title: "I Fixed a Problem!",
@@ -141,7 +148,8 @@ Blogs_and_posts_data.push({
     imgs: [
         "/Apps/Looper_comment/Images/logo.png",
         "/Blogs/Images/oiled_up/0.png"
-    ]
+    ],
+    tags: "WebDevelopment, Apps, AppDevelopment, Meme, VideoEditing, DaVinciResolve, Fun, YouTube"
 });
 Blogs_and_posts_data.push({
     title: "Why CODNEXT is Bad for it's Franchise",
@@ -166,5 +174,77 @@ Blogs_and_posts_data.push({
         "/Blogs/Images/cod_next/0.jpg",
         "/Blogs/Images/cod_next/2.jpg",
         "/Blogs/Images/cod_next/1.jpg",
-    ]
+    ],
+    tags: "COD, CallofDuty, VideoEditing, Video, YouTube, DaVinciResolve, Blender, VFX, Talking"
+});
+Blogs_and_posts_data.push({
+    title: "Low-Poly 3D Short Washroom Animation for a Video [Losing Aura at Domino's] using Blender",
+    subtitle: "I made this low poly short animation for some demonstration on my latest YouTube video. I also simulated water physics using Blender's physics system. Though I paused it and export it in the middle because I was running out of time. It was only for a video still. haha. \n\n You can watch the short animation on LinkedIn and you can also watch the full video on YouTube!",
+    date: "5th November 2024",
+    imgs: ["/Blogs/Images/dominos_video/5.jpg",
+        "/Projects/3D_projects/Images/dominos_room_act/0.jpg",
+        "/Projects/3D_projects/Images/dominos_room_act/1.jpg",
+        "/Projects/3D_projects/Images/dominos_room_act/2.jpg",
+        "/Projects/3D_projects/Images/dominos_room_act/3.jpg"
+    ],
+    btns: [
+        {
+            btn_string: "LinkedIn Video",
+            click_url: "https://www.linkedin.com/posts/realinspirer_blender-activity-7261064056529149952-vxFZ?utm_source=share&utm_medium=member_desktop",
+            external: true
+        },
+        {
+            btn_string: "YouTube Video",
+            click_url: "https://youtu.be/1xkqAfQTgCE?si=q5NsT2gSHGMNLphL",
+            external: true
+        }
+    ],
+    tags: "3D, Blender, LowPoly, Modelling, Shading, ShaderEditor, CutScene, VideoEditing, YouTube, Video"
+});
+Blogs_and_posts_data.push({
+    title: "Low-Poly 3D Short Car Animation for a Video [Losing Aura at Domino's] using Blender",
+    subtitle: "I made this low poly short animation for some demonstration on my latest YouTube video. I also used Blender's particles system to create some generic and simple smoke effect.\n\n You can view the short animation on LinkedIn or visit my YouTube for the full video!",
+    date: "5th November 2024",
+    imgs: ["/Projects/3D_projects/Images/dominos_car_act/0.jpg",
+        "/Blogs/Images/dominos_video/4.jpg",
+        "/Projects/3D_projects/Images/dominos_car_act/2.jpg",
+        "/Blogs/Images/dominos_video/0.png"
+    ],
+    btns: [
+        {
+            btn_string: "LinkedIn Video",
+            click_url: "https://www.linkedin.com/posts/realinspirer_animation-blender-activity-7259620621146316800-qAWQ?utm_source=share&utm_medium=member_desktop",
+            external: true
+        },
+        {
+            btn_string: "YouTube Video",
+            click_url: "https://youtu.be/1xkqAfQTgCE?si=q5NsT2gSHGMNLphL",
+            external: true
+        }
+    ],
+    tags: "3D, Blender, Video, VideoEditing, Cutscene, Modelling, Shader, ParticleSystem, YouTube"
+});
+Blogs_and_posts_data.push({
+    title: "Low-Poly 3D Area Design using Blender",
+    subtitle: "I made this low poly 3D build to use as a demonstration for my upcoming video. Details and perfection were not necessary as this is gonna be used for a small part in the video. Turned out to be quite good for my purpose. It was quite fun designing models with little time in hand.",
+    date: "10th October 2024",
+    imgs: ["/Projects/3D_projects/Images/low_poly/0.jpg",
+        "/Projects/3D_projects/Images/low_poly/1.jpg",
+        "/Projects/3D_projects/Images/low_poly/2.jpg",
+        "/Projects/3D_projects/Images/low_poly/3.jpg",
+        "/Projects/3D_projects/Images/low_poly/4.jpg",
+        "/Projects/3D_projects/Images/low_poly/5.jpg",
+        "/Projects/3D_projects/Images/low_poly/6.jpg",
+        "/Projects/3D_projects/Images/low_poly/7.jpg"
+    ],
+    tags: "3D, LowPoly, Blender, Modelling, Shading, Video, YouTube, Cutscene, VideoEditing"
+});
+Blogs_and_posts_data.push({
+    title: "Futuristic UI Design",
+    subtitle: "I made this futuristic using Blender. This design features various abstract elements made using simple shaders in Blender's shader editor to using some actual interesting geometry and shapes. \r\n \r\n I pulled an all nighter to finish this piece, and this now serves as a cover image for my various social pages like YouTube, Ko-fi, and much more!",
+    date: "8th October 2024",
+    imgs: ["/Projects/3D_projects/Images/fui/profile.jpg",
+        "/Projects/3D_projects/Images/fui/thumbnail.jpg"
+    ],
+    tags: "3D, Blender, VFX, CoverImages, Branding, Futuristic, GameDev, GameDevelopment, Unity"
 });
